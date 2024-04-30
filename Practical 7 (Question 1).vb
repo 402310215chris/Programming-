5d22e9262ffd105cdeccd89588d280bb719bfcac
Christine T Munhamo <164712654+402310215chris@users.noreply.github.com>
2024-04-30 10:24:13 +0200

    Add files via upload

---
+++ Practical 7 (Question 1).vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim hoursworked As Double
+        Dim hourlylrate As Double
+        Dim gross As Double
+        Dim deductions As Double
+        Dim nettpay As Double
+        hoursworked = Val(TextBox1.Text)
+        hourlylrate = Val(TextBox2.Text)
+        gross = hoursworked * hourlylrate
+        deductions = gross * 0.2
+        nettpay = gross - deductions
+        TextBox3.Text = gross
+        TextBox4.Text = deductions
+        TextBox5.Text = nettpay
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit()
+    End Sub
+End Class
